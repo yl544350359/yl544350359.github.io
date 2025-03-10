afc9f34f5d290745993d61daa68ca3990df3f467
unknown <yanli1030@outlook.com>
2025-03-10 21:44:37 +0900

    config tailwind

---
+++ last-war-supply-collection/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx,ts}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
